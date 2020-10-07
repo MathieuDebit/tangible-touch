e84fa691fa494ea6b5dd4d5a3ba0f21f64840042
Mathieu Débit <mathieu+github@debit.email>
2020-10-08 00:42:15 +0200

    Update example

--- src/App.js
+++ src/App.js
@@ -1,127 +1,118 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
-function startup() {
-  var el = document.getElementById("canvas");
-  el.addEventListener("touchstart", handleStart, false);
-  el.addEventListener("touchend", handleEnd, false);
-  el.addEventListener("touchcancel", handleCancel, false);
-  el.addEventListener("touchmove", handleMove, false);
-}
-var ongoingTouches = [];
-function handleStart(evt) {
-  evt.preventDefault();
-  console.log("touchstart.");
-  var el = document.getElementById("canvas");
-  var ctx = el.getContext("2d");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    console.log("touchstart:" + i + "...");
-    ongoingTouches.push(copyTouch(touches[i]));
-    var color = colorForTouch(touches[i]);
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.arc(touches[i].pageX, touches[i].pageY, 50, 0, 2 * Math.PI, false);  // a circle at the start
-    ctx.fillStyle = color;
-    ctx.stroke();
-    console.log("touchstart:" + i + ".");
-  }
-}
-function handleMove(evt) {
-  evt.preventDefault();
-  var el = document.getElementById("canvas");
-  var ctx = el.getContext("2d");
-  var touches = evt.changedTouches;
-
-  log('touches.length ' + touches.length);
-  for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      console.log("continuing touch "+idx);
-      ctx.beginPath();
-      ctx.clearRect(touches[i].pageX - 100, touches[i].pageY - 100, 200, 200);
-      ctx.arc(touches[i].pageX, touches[i].pageY, 50, 0, 2 * Math.PI, false);  // a circle at the start
-      ctx.stroke();
-
-      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-      console.log(".");
-    } else {
-      console.log("can't figure out which touch to continue");
-    }
-  }
-}
-function handleEnd(evt) {
-  evt.preventDefault();
-  log("touchend");
-  var el = document.getElementById("canvas");
-  var ctx = el.getContext("2d");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      ctx.clearRect(0, 0, el.width, el.height);
-      ongoingTouches.splice(idx, 1);  // remove it; we're done
-    } else {
-      console.log("can't figure out which touch to end");
-    }
-  }
-}
-function handleCancel(evt) {
-  evt.preventDefault();
-  console.log("touchcancel.");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-    ongoingTouches.splice(idx, 1);  // remove it; we're done
-  }
-}
-function colorForTouch(touch) {
-  var r = touch.identifier % 16;
-  var g = Math.floor(touch.identifier / 3) % 16;
-  var b = Math.floor(touch.identifier / 7) % 16;
-  r = r.toString(16); // make it a hex digit
-  g = g.toString(16); // make it a hex digit
-  b = b.toString(16); // make it a hex digit
-  var color = "#" + r + g + b;
-  console.log("color for touch with identifier " + touch.identifier + " = " + color);
-  return color;
-}
-function copyTouch({ identifier, pageX, pageY }) {
-  return { identifier, pageX, pageY };
-} function ongoingTouchIndexById(idToFind) {
-  for (var i = 0; i < ongoingTouches.length; i++) {
-    var id = ongoingTouches[i].identifier;
-
-    if (id == idToFind) {
-      return i;
-    }
-  }
-  return -1;    // not found
-}
-function log(msg) {
-  var p = document.getElementById('log');
-  p.innerHTML = msg;
-}
-
 function App() {
+  const canvas = useRef(null);
+
+  const [touches, setTouches] = useState([]);
+
   useEffect(() => {
-    startup();
+    const canvasEl = canvas.current;
+
+    canvasEl.addEventListener('touchstart', handleStart, false);
+    canvasEl.addEventListener('touchend', handleEnd, false);
+    canvasEl.addEventListener('touchcancel', handleCancel, false);
+    canvasEl.addEventListener('touchmove', handleMove, false);
+
+    return () => {
+      canvasEl.removeEventListener('touchstart', handleStart);
+      canvasEl.removeEventListener('touchend', handleEnd);
+      canvasEl.removeEventListener('touchcancel', handleCancel);
+      canvasEl.removeEventListener('touchmove', handleMove);
+    };
   });
+
+  function handleStart(event) {
+    event.preventDefault();
+
+    const context = canvas.current.getContext('2d');
+    const changedTouches = event.changedTouches;
+    setTouches(() => event.touches);
+    console.log('handleStart', event.touches);
+
+    for (let i = 0; i < changedTouches.length; i++) {
+      const color = colorForTouch(changedTouches[i]);
+      context.beginPath();
+      context.lineWidth = 5;
+      context.arc(changedTouches[i].pageX, changedTouches[i].pageY, 50, 0, 2 * Math.PI, false);
+      context.fillStyle = color;
+      context.stroke();
+      log('touchstart: ' + i);
+    }
+
+  }
+
+  function handleMove(event) {
+    event.preventDefault();
+
+    const context = canvas.current.getContext('2d');
+    const changedTouches = event.changedTouches;
+    setTouches(() => event.touches);
+
+    for (let i = 0; i < changedTouches.length; i++) {
+      const color = colorForTouch(changedTouches[i]);
+      context.beginPath();
+      context.clearRect(changedTouches[i].pageX - 100, touches[i].pageY - 100, 200, 200);
+      context.arc(changedTouches[i].pageX, changedTouches[i].pageY, 50, 0, 2 * Math.PI, false);
+      context.stokeStyle = color;
+      context.stroke();
+    }
+  }
+
+  function handleEnd(event) {
+    event.preventDefault();
+
+    const context = canvas.current.getContext('2d');
+    const changedTouches = event.changedTouches;
+    setTouches(() => event.touches);
+    console.log('handleEnd', event.touches);
+
+    for (let i = 0; i < changedTouches.length; i++) {
+      log('touchend: ' + i);
+      // const color = colorForTouch(touches[i]);
+      context.clearRect(changedTouches[i].pageX - 55, changedTouches[i].pageY - 55, 110, 110);
+    }
+  }
+
+  function handleCancel(event) {
+    event.preventDefault();
+
+    log('touchcancel');
+
+    const changedTouches = event.changedTouches;
+    setTouches(() => event.touches);
+
+    for (let i = 0; i < changedTouches.length; i++) {
+    }
+  }
+
+  function colorForTouch(touch) {
+    let r = touch.identifier % 16;
+    let g = Math.floor(touch.identifier / 3) % 16;
+    let b = Math.floor(touch.identifier / 7) % 16;
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    const color = '#' + r + g + b;
+    // log('color for touch with identifier ' + touch.identifier + ': ' + color);
+    return color;
+  }
+
+  function log(msg) {
+    const p = document.getElementById('log');
+    p.innerHTML = msg + '<br>' + p.innerHTML;
+  }
 
   return (
     <div className="App">
-      <canvas id="canvas" width={window.innerWidth} height={window.innerHeight}>
+      <canvas ref={canvas} width={window.innerWidth} height={window.innerHeight}>
         Your browser does not support canvas element.
       </canvas>
       <br/>
-      <pre id="log">Ready</pre>
+      <div className="box">
+        <h1>{touches.length}</h1>
+        <pre id="log">Ready</pre>
+      </div>
     </div>
   );
 }
