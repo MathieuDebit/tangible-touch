992be4210d79c27455012b1f7d1dde0b522d69cf
Mathieu Débit <mathieu+github@debit.email>
2020-10-07 09:40:04 +0200

    Updates

---
+++ precache-manifest.6698ee0d1b446fc1d4a8b4ed8b8a6dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138d4153378d1e46123cab9d77fe0fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27ea6c3cd64acdb02ad2",
+    "url": "/static/css/main.4d70f2f8.chunk.css"
+  },
+  {
+    "revision": "72a73e8e1060e868d2c8",
+    "url": "/static/js/2.a9f3cc90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9f3cc90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ea6c3cd64acdb02ad2",
+    "url": "/static/js/main.657d8225.chunk.js"
+  },
+  {
+    "revision": "e883b898c167eb414ca9",
+    "url": "/static/js/runtime-main.ad7a6843.js"
+  }
+]);
